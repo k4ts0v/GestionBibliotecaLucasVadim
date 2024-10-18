a2e1e24bf2c856e3cb043d15331caa55b7b8939b
VadimGr74 <fs92296@gmail.com>
2024-10-18 10:27:40 +0200

    test

--- biblioteca_jdbc/src/main/java/com/lvgvg/Main.java
+++ biblioteca_jdbc/src/main/java/com/lvgvg/Main.java
@@ -3,5 +3,7 @@ package com.lvgvg;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("asad");
+        System.out.println("testd");
     }
 }

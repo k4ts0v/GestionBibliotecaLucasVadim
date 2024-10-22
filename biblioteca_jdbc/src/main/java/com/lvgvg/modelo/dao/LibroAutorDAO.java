6b46510578a28099d05267e36ca180dcaa5283d3
Lucas V. <77939384+k4ts0v@users.noreply.github.com>
2024-10-22 08:49:37 +0200

    feat: añadido de la entidad LibroAutor y su correspondiente DAO.

---
+++ biblioteca_jdbc/src/main/java/com/lvgvg/modelo/dao/LibroAutorDAO.java
@@ -0,0 +1,146 @@
+/*
+ * File: LibroAutorDAO.java
+ * Project: biblioteca_jdbc
+ * File Created: Tuesday, 22nd October 2024 8:31:22 AM
+ * Author: Lucas Villa (k4ts0v@protonmail.com)
+ * -----
+ * Last Modified: Tuesday, 22nd October 2024 8:46:33 AM
+ * Modified By: Lucas Villa (k4ts0v@protonmail.com)
+ */
+
+
+
+package com.lvgvg.modelo.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.ArrayList;
+
+import com.lvgvg.modelo.dto.LibroAutor;
+
+public class LibroAutorDAO implements DAO<LibroAutor> {
+    private Connection conexion = JDBC.getConexion();
+    private final String CREATE = "INSERT INTO LibroAutor VALUES(?,?,?)";
+    private final String READ = "SELECT * FROM LibroAutor WHERE id = ?";
+    private final String READALL = "SELECT * FROM LibroAutor";
+    private final String UPDATE = "UPDATE LibroAutor SET titulo=?, isbn=? WHERE id = ?";
+    private final String DELETE = "DELETE * FROM LibroAutor WHERE id = ?";
+
+    /**
+     * Este método añade un libroAutor a la BD.
+     * @param j Objeto libroAutor a añadir.
+     * @return Devuelve 1 si la operación se ha realizado correctamente.
+     * @throws SQLException Lanza una SQLException si no se ha podido crear el libroAutor en la BD.
+     */
+    @Override
+    public Integer create(LibroAutor l) throws SQLException {
+        try {
+            PreparedStatement ps = conexion.prepareStatement(CREATE, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, l.getIdLibro());
+            ps.setInt(2, l.getIdAutor());
+
+            ps.executeUpdate(CREATE);
+            ResultSet rs = ps.getGeneratedKeys();
+            while (rs.next()) {
+                l.setIdLibro(rs.getInt(1));
+                l.setIdAutor(rs.getInt(2));
+                return 1;
+            }
+        } catch (SQLException e) {
+            throw new SQLException();
+        }
+        return -1;
+    }
+
+    /**
+     * Este método lee el libroAutor especificado (por ID) de la BD.
+     * @param id ID del libroAutor.
+     * @return El objeto libroAutor que ha sido leído.
+     * @throws SQLException Lanza una SQLException si no se ha podido leer el libroAutor de la BD.
+     */
+    @Override
+    public LibroAutor read(LibroAutor la) throws SQLException {
+        try  {
+            PreparedStatement ps = conexion.prepareStatement(READ);
+            ps.setInt(1, la.getIdLibro());
+            ps.setInt(2, la.getIdAutor());
+            ResultSet rs = ps.executeQuery(READ);
+            LibroAutor l = getLibroAutorRS(rs);
+            return l;
+        } catch (SQLException e) {
+            throw new SQLException();
+        }
+    }
+
+    /**
+     * Este método lee todos los libroAutors que existen en la BD.
+     * @return ArrayList<LibroAutor> Lista de los libroAutors existentes en la BD.
+     * @throws SQLException Lanza una SQLException si no se ha podido leer el contenido de la BD.
+     */
+    @Override
+    public ArrayList<LibroAutor> readAll() throws SQLException {
+        ArrayList<LibroAutor> listaLibroAutores = new ArrayList<>();
+        try {
+            Statement st = conexion.createStatement();
+            ResultSet rs = st.executeQuery(READALL);
+            while (rs.next()) {
+                listaLibroAutores.add(getLibroAutorRS(rs));
+            }
+            return listaLibroAutores;
+        } catch (SQLException e) {
+            throw new SQLException();
+        }
+    }
+
+    /**
+     * Este método actualiza un libroAutor de la BD en base a su ID.
+     * @param l LibroAutor a actualizar.
+     * @return Devuelve 1 si se ha actualizado correctamente.
+     * @throws SQLException Lanza una SQLException si no se ha podido actualizar el libroAutor.
+     */
+    @Override
+    public Integer update(LibroAutor l) throws SQLException {
+        try {
+            PreparedStatement ps = conexion.prepareStatement(UPDATE);
+            LibroAutor alBD = read(l);
+            ps.setInt(1, alBD.getIdLibro());
+            ps.setInt(2, alBD.getIdAutor());
+            ps.execute();
+            return 1;
+        } catch (SQLException e) {
+            throw new SQLException();
+        }
+    }
+
+    /**
+     * Este método borra un libroAutor de la BD en base a su ID.
+     * @param l LibroAutor a borrar.
+     * @return Devuelve 1 si el libroAutor se ha borrado correctamente.
+     * @throws SQLException Lanza una SQLException si no se ha podido borrar el libroAutor.
+     */
+    @Override
+    public Integer delete(LibroAutor l) throws SQLException {
+        try {
+            PreparedStatement ps = conexion.prepareStatement(DELETE);
+            ps.setInt(1, l.getIdLibro());
+            ps.setInt(2, l.getIdAutor());
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new SQLException();
+        }
+    }
+
+    /**
+     * Este método genera un libroAutor en base al ResultSet devuelto por una consulta.
+     * @param rs - ResultSet de la consulta.
+     * @return Objeto libroAutor con los datos de la consulta.
+     * @throws SQLException Lanza una SQLException si no se ha podido crear el libroAutor.
+     */
+    public LibroAutor getLibroAutorRS(ResultSet rs) throws SQLException {
+        return new LibroAutor(rs.getInt(1), rs.getInt(2));
+    }
+}
---
+++ biblioteca_jdbc/src/main/java/com/lvgvg/modelo/dto/LibroAutor.java
@@ -0,0 +1,92 @@
+package com.lvgvg.modelo.dto;
+
+import java.util.Objects;
+
+// Clase LibroAutor, que sirve como tabla intermedia para la relación entre Libro y Autor.
+public class LibroAutor {
+    private Integer idAutor; // Id del autor.
+    private Integer idLibro; // Id del libro.
+
+    public LibroAutor(Integer idAutor, Integer idLibro) {
+        setIdAutor(idAutor); // Id del autor.
+        setIdLibro(idLibro); // Id del libro.
+    }
+
+    // Getters y setters.
+
+    /**
+     * Getter que devuelve el id del autor.
+     *
+     * @return Id del autor.
+     */
+    public Integer getIdAutor() {
+        return idAutor;
+    }
+
+    /**
+     * Setter que asigna el id del autor a una instancia de LibroAutor.
+     *
+     * @param id Id del autor a asignar.
+     */
+    public void setIdAutor(Integer idAutor) {
+        this.idAutor = idAutor;
+    }
+
+    /**
+     * Getter que devuelve el id del libro.
+     *
+     * @return Id del libro.
+     */
+    public Integer getIdLibro() {
+        return idLibro;
+    }
+
+    /**
+     * Setter que asigna el id del libro a una instancia de LibroAutor.
+     *
+     * @param id Id del libro a asignar.
+     */
+    public void setIdLibro(Integer idLibro) {
+        this.idLibro = idLibro;
+    }
+
+    /**
+     * Este método genera el hashcode, que es un integer generado por un algoritmo
+     * de hashing. Para ello, ha usado ambos IDs.
+     *
+     * @return int - Hashcode generado en base los IDs.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(idAutor, idLibro);
+    }
+
+    /**
+     * Compara dos objetos para ver si son iguales.
+     * En este caso, solo son iguales si son dos instancias de LibroAutor y poseen los
+     * mismos IDs.
+     *
+     * @param e0 - La otra instancia de libro a usar para la comparación.
+     * @return boolean - Si son iguales, devuelve verdadero, si no, falso.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof LibroAutor))
+            return false;
+        LibroAutor other = (LibroAutor) obj;
+        return Objects.equals(idAutor, other.idAutor) && Objects.equals(idLibro, other.idLibro);
+    }
+
+    /**
+     * Método toString que genera una String con toda la información del objeto.
+     *
+     * @return String formateada con toda la información de la instancia de la clase
+     *         LibroAutor.
+     */
+    @Override
+    public String toString() {
+        return String.format("LibroAutor [idAutor=%s, idLibro=%s]", idAutor, idLibro);
+    }
+}
